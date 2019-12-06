22577f8de10ac98da56c38ed05af499c02bb3f6f
Treek <nbatre1@yahoo.com>
2019-12-06 00:39:10 -0600

    keep moving forward

--- reactair/src/index.js
+++ reactair/src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
-import {createStore,applyMiddle} from 'redux';
+import {createStore,applyMiddleware} from 'redux';
+import rootReducer from './reducers/rootReducer';
+import reduxPromise from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const theStore = applyMiddleware(reduxPromise)(createStore)(rootReducer);
+
+ReactDOM.render(
+<Provider store={theStore}>
+<App />
+</Provider>, document.getElementById('root'));
 
--- reactair/src/reducers/authReducer.js
+++ reactair/src/reducers/authReducer.js
@@ -1,7 +1,8 @@
 export default(state ={},action)=>{
 
 
+    return state
 
 
-    
+
 }
