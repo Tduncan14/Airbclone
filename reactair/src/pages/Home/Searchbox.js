c275e37b37494ce90a4e4d1052d874d1d171b3e7
Treek <nbatre1@yahoo.com>
2019-12-07 17:08:19 -0600

    keep moving forward

--- reactair/src/pages/Home/Searchbox.js
+++ reactair/src/pages/Home/Searchbox.js
@@ -3,12 +3,30 @@ import './search.css'
 
 class SearchBox extends Component {
 
+      constructor(props){
+          super(props)
 
 
+          this.state={
+            
+            where:''
+          }
+      }
+
+
+     changeWhere = (event) =>{
+
+      this.setState({
+          where:event.target.value
+      })
+
+
+
+      }
 
  render(){
 
-
+    console.log(this.state.where,"state");
 
     return(
         <div>
@@ -16,6 +34,20 @@ class SearchBox extends Component {
             <h1>
                 Book unique places to stay and things to do
             </h1>
+
+            <form className="search-box-form">
+                <div className="col m12">
+
+                  <div className="input-field" id="where" >
+                      <input onChange={this.changeWhere} 
+                      placeholder="Anywhere"
+                      value={this.state.where}
+                      />
+
+                  </div>
+                </div>
+            
+            </form>
            </div>
         </div>
     )
