59a210d032cac72da6121cee8f9ef48100bc3b0d
Treek <nbatre1@yahoo.com>
2019-12-09 09:52:44 -0600

    keep moving forward

--- reactair/src/pages/Home/Home.js
+++ reactair/src/pages/Home/Home.js
@@ -2,24 +2,41 @@ import React ,{Component} from 'react';
 import './home.css'
 import SearchBox from './Searchbox';
 import axios from 'axios'
+import Spinner from '../../utility/Spinner/Spinner';
 
 const apiCall='https://airbnb-api.robertbunch.dev'
 
 
 class Home extends Component {
 
-''
+    constructor(props){
+        super(props)
+
+        this.state ={
+            cities:[]
+        }
+    }
+
 
     async componentDidMount(){
 
     const recommend =  await axios.get(`${apiCall}/cities/recommended`)
 
     console.log(recommend.data)
-  
+   
+    this.setState({
+        cities:recommend.data
+    })
 
    }
 
  render(){
+     console.log(this.state.cities,'state')
+
+     if(this.state.cities.length === 0){
+
+        return <Spinner />
+     }
     return(
         <div className="container-fluid">
             <div className="row">
