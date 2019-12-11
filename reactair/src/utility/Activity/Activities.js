7173089ee933101267bf36e357ba7029fd32112d
Treek <nbatre1@yahoo.com>
2019-12-11 16:56:30 -0600

    keep moving forward

---
+++ reactair/src/utility/Activity/Activities.js
@@ -0,0 +1,27 @@
+import React ,{Component} from 'react';
+import Activity from './Activity';
+import './Activity.css';
+
+
+
+class Activities extends Component {
+
+
+
+
+
+
+    render(){
+
+
+        return(
+            <div>
+                Activities
+            </div>
+        )
+    }
+}
+
+
+
+export default Activities;
---
+++ reactair/src/utility/Activity/Activity.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+import './Activity.css';
+
+
+class Activity extends Component {
+
+
+
+
+
+    render(){
+        return(
+
+          <div>
+              Act
+          </div>
+        )
+    }
+}
+
+
+export default Activity;
